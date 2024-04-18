566e9c84446a238c60f44805aafaaee4654f40d5
zainmk <zain.khan@heliolytics.com>
2024-04-17 21:46:01 -0400

    exercise03

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,8 +3,10 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../workerized-filter-cities'
+// import {getItems} from '../workerized-filter-cities'
+import {getItems} from '../filter-cities'
 import {useAsync, useForceRerender} from '../utils'
+
 
 function Menu({
   items,
@@ -21,8 +23,8 @@ function Menu({
           getItemProps={getItemProps}
           item={item}
           index={index}
-          selectedItem={selectedItem}
-          highlightedIndex={highlightedIndex}
+          isSelected={selectedItem?.id === item.id}
+          isHighlighted={highlightedIndex === index}
         >
           {item.name}
         </ListItem>
@@ -30,18 +32,20 @@ function Menu({
     </ul>
   )
 }
+Menu = React.memo(Menu)
 // 🐨 Memoize the Menu here using React.memo
 
 function ListItem({
   getItemProps,
   item,
   index,
-  selectedItem,
-  highlightedIndex,
+  isSelected,
+  isHighlighted,
   ...props
 }) {
-  const isSelected = selectedItem?.id === item.id
-  const isHighlighted = highlightedIndex === index
+  // extra credit - 2
+  // const isSelected = selectedItem?.id === item.id
+  // const isHighlighted = highlightedIndex === index
   return (
     <li
       {...getItemProps({
@@ -57,15 +61,35 @@ function ListItem({
   )
 }
 // 🐨 Memoize the ListItem here using React.memo
+// ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+
+//   // extra credit - 1
+//   if (prevProps.getItemProps !== nextProps.getItemProps) return false
+//   if (prevProps.item !== nextProps.item) return false
+//   if (prevProps.index !== nextProps.index) return false
+//   if (prevProps.selectedItem !== nextProps.selectedItem) return false
+
+//   if(prevProps.highlightedIndex !== nextProps.highlightedIndex){
+//     const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
+//     const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
+//     return wasPrevHighlighted === isNowHighlighted
+//   }
+
+// })
+
+// extra credit - 2
+ListItem = React.memo(ListItem)
 
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
-  React.useEffect(() => {
-    run(getItems(inputValue))
-  }, [inputValue, run])
+  const allItems = React.useMemo(()=> getItems(inputValue), [inputValue])
+
+  //const {data: allItems, run} = useAsync({data: [], status: 'pending'})
+  // React.useEffect(() => {
+  //   run(getItems(inputValue))
+  // }, [inputValue, run])
   const items = allItems.slice(0, 100)
 
   const {
